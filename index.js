71f09a3583933d543fc79d4efc2174a5988f80f7
Sonoslokos <nicolodi098@gmail.com>
2021-09-01 21:40:41 -0300

    Clean trash

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ app.get('/', function (req, res) {
 })
 
 app.get('/process_get', function (req, res) {
-   // Prepare output in JSON format
    response = {
       first_name:req.query.first_name,
       last_name:req.query.last_name
@@ -22,32 +21,3 @@ var server = app.listen(8000, function () {
    
    console.log("Example app listening at http://%s:%s", host, port)
 })
-
-
-
-
-
-
-
-
-
-
-
-// // vamos importar os módulos http e filesystem
-// const http = require('http'), fs = require('fs')
-
-// // então, criamos o servidor http
-// http.createServer((req, res) => {
-//   // esse é o cabeçalho da nossa resposta
-//   res.writeHead(200, {
-//     'Content-Type': 'text/html',
-//     'Access-Control-Allow-Origin' : '*'
-//   })
-//   // leremos o index.html
-//   let readStream = fs.createReadStream(__dirname + '/src/dragFile.html')
-//   // então, enviamos ele para o cliente
-//   readStream.pipe(res)
-// }).listen(8000)
-
-// // no fim, informamos o endpoint para o usuário
-// console.log('Visite-me em: http://localhost:8000')
